bff059d6453cf544953c06042831ad0281cc4459
Likith-Reddy25 <likithreddy2508@gmail.com>
2026-01-07 21:53:03 +0530

    Updated Backend

--- Backend/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ Backend/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazystore.controller;
 
+import com.eazybytes.eazystore.dto.ErrorResponseDto;
 import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.entity.Product;
 import com.eazybytes.eazystore.repository.ProductRepository;
@@ -9,11 +10,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -29,5 +29,14 @@ public class ProductController {
 //        Thread.sleep(3000);
         List<ProductDto> productList= iProductService.getProducts();
         return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                  WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false), HttpStatus.SERVICE_UNAVAILABLE,
+                exception.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
