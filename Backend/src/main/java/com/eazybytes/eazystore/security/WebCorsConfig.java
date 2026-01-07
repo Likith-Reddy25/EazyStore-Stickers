465c1d1f9bb74c294ba01a9e23031134a5269f4e
Likith-Reddy25 <likithreddy2508@gmail.com>
2026-01-07 18:23:27 +0530

    Updated Backend

--- Backend/src/main/java/com/eazybytes/eazystore/security/WebCorsConfig.java
+++ Backend/src/main/java/com/eazybytes/eazystore/security/WebCorsConfig.java
@@ -20,7 +20,7 @@ public class WebCorsConfig {
 
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
-                "https://eazystore-stickers.onrender.com/api/v1"
+                "https://eazystore-stickers.onrender.com"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
