f39b589aae992c5b2345b3283950dba4ae0a5e8c
Likith-Reddy25 <likithreddy2508@gmail.com>
2026-01-08 09:10:46 +0530

    Updated Backend

--- Backend/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ Backend/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -68,7 +68,7 @@ public class EazyStoreSecurityConfig {
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
         );
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
